778807221585dd54a1fab942650cd9cc6dcc9f9a
JustAsh123 <ashmit1268@gmail.com>
2025-06-04 11:55:49 +0530

    profile page update and font

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth.jsx";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
